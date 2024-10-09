a0e59c7e3182f8d95892155707d6d35929e1cfc1
Oksana <benefit989@gmail.com>
2024-10-09 20:17:22 +0200

    fix

--- src/components/day/Day.jsx
+++ src/components/day/Day.jsx
@@ -1,9 +1,9 @@
-import React, { memo } from "react";
-import PropTypes from "prop-types";
-import Hour from "../hour/Hour";
-import TimeScale from "../timescale/TimeScale";
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import Hour from '../hour/Hour';
+import TimeScale from '../timescale/TimeScale';
 
-import "./day.scss";
+import './day.scss';
 
 const Day = ({ dataDay, dayEvents, isToday }) => {
   const hours = Array.from({ length: 24 }, (_, index) => index);
@@ -11,14 +11,10 @@ const Day = ({ dataDay, dayEvents, isToday }) => {
   return (
     <div className="calendar__day" data-day={dataDay}>
       {isToday && <TimeScale />}
-      {hours.map((hour) => {
-        const hourEvents = dayEvents.filter(
-          (event) => event.dateFrom.getHours() === hour,
-        );
+      {hours.map(hour => {
+        const hourEvents = dayEvents.filter(event => event.dateFrom.getHours() === hour);
 
-        return (
-          <Hour key={dataDay + hour} dataHour={hour} hourEvents={hourEvents} />
-        );
+        return <Hour key={dataDay + hour} dataHour={hour} hourEvents={hourEvents} />;
       })}
     </div>
   );
